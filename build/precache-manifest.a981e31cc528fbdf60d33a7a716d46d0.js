ce0c0521116c8a27c48eef4b1cdd350a3eb173e9
bui thuan <thithuan.bui@jp.ey.com>
2020-02-26 17:19:41 +0900

    deploy added

---
+++ build/precache-manifest.a981e31cc528fbdf60d33a7a716d46d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68f2add52a3d19a5fe4590aa9fe8e9c",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/robofriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3fab74a856728937b1e6",
+    "url": "/robofriend/static/css/main.6979342c.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/robofriend/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriend/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fab74a856728937b1e6",
+    "url": "/robofriend/static/js/main.c350c4e5.chunk.js"
+  },
+  {
+    "revision": "73b0268b71309baed9b7",
+    "url": "/robofriend/static/js/runtime-main.2d85b6a6.js"
+  },
+  {
+    "revision": "c75f490e18d8ca922a4648ab80dc5089",
+    "url": "/robofriend/static/media/Strawberry Shortcake Supplement Logo.c75f490e.ttf"
+  }
+]);
